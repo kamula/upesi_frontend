a45e90cb2637ed7f8bda39543c13941cc67f2db1
kamula <kamulaisaac@gmail.com>
2024-04-20 05:55:08 +0300

    react router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Root from './Root'
+import LoginScreen from './screens/loginScreen/LoginScreen'
+import RegisterScreen from './screens/registerScreen/RegisterScreen'
+import DashboardScreen from './screens/dashboardScreen/DashboardScreen'
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root />}>
+      <Route index element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/dashboard' element={<DashboardScreen />} />
+    </Route>
+  )
+)
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/screens/loginScreen/LoginScreen.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../../components/loginScreenComponents/LoginForm"
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default LoginScreen
